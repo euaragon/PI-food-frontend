7e715abca609db612116623f6011c23cd3aa5180
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-03-19 14:09:54 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -2,29 +2,27 @@ import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Landing from "./components/Landing";
 import Home from "./components/Home";
-import FoodCreate from "./components/FoodCreate"
-import Nav from './components/Nav';
-import Detail from './components/Detail'
-import axios from 'axios'
+import FoodCreate from "./components/FoodCreate";
+import Nav from "./components/Nav";
+import Detail from "./components/Detail";
+import axios from "axios";
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://pi-food-back-production-ec1c.up.railway.app/';
+axios.defaults.baseURL = "https://pi-food-back-production-97f8.up.railway.app";
 
 function App() {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
-
     <div className="App">
-      {location.pathname !== "/" && <Nav/>}
+      {location.pathname !== "/" && <Nav />}
       <Routes>
-        <Route exact path="/" element={<Landing/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/foodcreate" element={<FoodCreate/>} />
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/foodcreate" element={<FoodCreate />} />
         <Route path="/recipes/:recipeId" element={<Detail />} />
       </Routes>
     </div>
-
   );
 }
 
